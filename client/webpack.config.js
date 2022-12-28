1fd71931e0ab43654832e206aba8e2b7b70234cc
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-28 09:49:21 -0500

    server working working on client errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,16 +21,18 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Text Editor',
-        favicon: './favico'
+        favicon: './favicon.ico'
       }),
       new InjectManifest({
-        swSrc: '.sw.js',
+        swSrc: './src-sw.js',
         swDest: 'service-worker.js'
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
-        description: "Text editor ",
+        description: "write down notes.",
         background_color:'#225ca3',
         theme_color: "#225ca3",
         start_url: "/",
